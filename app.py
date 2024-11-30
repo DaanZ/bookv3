8327155c43ad25724a2986636ffc10f8f97868a7
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-11-30 16:42:36 +0700

    Variable Chunks

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import copy
-import math
 import os
 
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
+from chunk_size import get_page_chunks
 from read_fragments import read_book_pages, highlight_chunk
 
 
 def reset_app():
-    for key in st.session_state.keys():
-        del st.session_state[key]
     st.session_state.pages = None
-    st.write("<script>location.reload()</script>", unsafe_allow_html=True)
+    st.session_state.chunks = None
+    st.session_state.page_chunks = None
+    st.session_state.current_index = 0
+    st.session_state.next_highlighted = None
+    st.session_state.progress_container = st.empty()  # Create an empty container for the progress bar
+    st.session_state.text_box = st.empty()
+    st.session_state.button_container = st.empty()  # Create an empty container for the Next button
+    st.session_state.first_chunk = True  # Create an empty container for the Next button
+    st.session_state.amount_chunks = 10  # Create an empty container for the Next button
 
 
 # Render the chunk with custom CSS
@@ -57,34 +63,12 @@ def render_chunk(text_box, chunk):
 
 # Streamlit Interface
 if 'pages' not in st.session_state:
-    st.session_state.pages = None
-if 'chunks' not in st.session_state:
-    st.session_state.chunks = None
-
-if 'current_index' not in st.session_state:
-    st.session_state.current_index = 0
-
-if 'next_highlighted' not in st.session_state:
-    st.session_state.next_highlighted = None
-
-if 'progress_container' not in st.session_state:
-    st.session_state.progress_container = st.empty()  # Create an empty container for the progress bar
-
-if 'text_box' not in st.session_state:
-    st.session_state.text_box = st.empty()
-
-if 'button_container' not in st.session_state:
-    st.session_state.button_container = st.empty()  # Create an empty container for the Next button
-
-if 'first_chunk' not in st.session_state:
-    st.session_state.first_chunk = True  # Create an empty container for the Next button
+    reset_app()
 
 
-chunks = 10
-
-# File upload
-if st.session_state.pages is None:
+def upload_stage():
     st.title("Book Summarized Viewer")
+    st.session_state.amount_chunks = st.number_input("Number of parts", 5, 25, st.session_state.amount_chunks)
     uploaded_file: UploadedFile = st.file_uploader("Upload a Book file (pdf)", type="pdf")
     if uploaded_file:
         with st.spinner("Reading Book..."):
@@ -94,38 +78,46 @@ if st.session_state.pages is None:
                 f.write(uploaded_file.getbuffer())
 
             st.session_state.pages = read_book_pages(file_path)
-            st.session_state.chunk_size = math.floor(len(st.session_state.pages) / chunks)
+            st.session_state.page_chunks = get_page_chunks(st.session_state.pages, st.session_state.amount_chunks)
 
         st.success(f"Read book with {len(st.session_state.pages)} pages!")
 
 
-if st.session_state.pages:
-
-    st.session_state.progress_container.progress((st.session_state.current_index + 1) / chunks)
-
-    if st.session_state.next_highlighted is None:
+def reading_stage():
+    st.session_state.progress_container.progress((st.session_state.current_index + 1) / st.session_state.amount_chunks)
+    print("next", st.session_state.next_highlighted)
+    if st.session_state.next_highlighted:
+        highlighted = copy.deepcopy(st.session_state.next_highlighted)
+        st.session_state.next_highlighted = None
+    else:
         with st.spinner("Highlighting chunk..."):
-            s = st.session_state.current_index * st.session_state.chunk_size
-            page_chunk = st.session_state.pages[s:s + st.session_state.chunk_size]
+            page_chunk = st.session_state.page_chunks[st.session_state.current_index]
             print(st.session_state.first_chunk)
             highlighted = highlight_chunk(page_chunk, st.session_state.first_chunk)
             st.session_state.first_chunk = False
-    else:
-        highlighted = copy.deepcopy(st.session_state.next_highlighted)
-        st.session_state.next_highlighted = None
 
     print("Highlighted", highlighted)
     if highlighted:
         render_chunk(st.session_state.text_box, highlighted)
 
-    print(st.session_state.current_index, chunks - 1)
-    if st.session_state.current_index < chunks - 1:
-        s = (st.session_state.current_index+1)*st.session_state.chunk_size
-        page_chunk = st.session_state.pages[s:s+st.session_state.chunk_size]
-        st.session_state.next_highlighted = highlighted = highlight_chunk(page_chunk, st.session_state.first_chunk)
+    print(st.session_state.current_index, st.session_state.amount_chunks - 1)
+    if st.session_state.current_index < st.session_state.amount_chunks - 1:
+        page_chunk = st.session_state.page_chunks[st.session_state.current_index + 1]
+        st.session_state.next_highlighted = highlight_chunk(page_chunk, st.session_state.first_chunk)
 
         if st.session_state.button_container.button("Next"):
             st.session_state.current_index += 1
 
     elif st.session_state.button_container.button("Summarize another Book"):
+        st.session_state.pages = None
         reset_app()
+
+
+if __name__ == "__main__":
+    # File upload
+    if st.session_state.pages is None:
+        upload_stage()
+
+    # check again if the pages exist now.
+    if st.session_state.pages:
+        reading_stage()
